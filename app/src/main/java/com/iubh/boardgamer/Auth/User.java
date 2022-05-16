9d408db3d648ae37b1c9412dc5d0b26493240473
StefanHausruckinger <stefan.hausruckinger@iubh-fernstudium.de>
2022-05-16 23:16:19 +0200

    Auth Firebase with Realtime Database and java

---
+++ app/src/main/java/com/iubh/boardgamer/Auth/User.java
@@ -0,0 +1,15 @@
+package com.iubh.boardgamer.Auth;
+
+public class User {
+
+    public String fullName, email;
+
+    public User(){
+
+    }
+
+    public User(String fullName, String email){
+        this.fullName = fullName;
+        this.email = email;
+    }
+}
